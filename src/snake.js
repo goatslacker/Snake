9c8560a50f0a9ec6ede35bb672cf0e80b50774a1
Josh Perez <josh@bigstar.tv>
2011-02-01 10:36:41 -0500

    removed inheritance, added it to snake.base, experimenting

--- src/snake.js
+++ src/snake.js
@@ -42,74 +42,6 @@ String.prototype.interpose = function (foreign) {
   return str;
 };
 
-// TODO
-// jsDoc
-
-// jslint comply this?
-/* Simple JavaScript Inheritance
- * By John Resig http://ejohn.org/
- * MIT Licensed.
- */
-// Inspired by base2 and Prototype
-(function(){
-  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-  // The base Snake.Class implementation (does nothing)
-  Snake.Class = function(){};
-  
-  // Create a new Snake.Class that inherits from this class
-  Snake.Class.extend = function(prop) {
-    var _super = this.prototype;
-    
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
-    
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" && 
-        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this._super;
-            
-            // Add a new ._super() method that is the same method
-            // but on the super-class
-            this._super = _super[name];
-            
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
-            var ret = fn.apply(this, arguments);        
-            this._super = tmp;
-            
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
-    }
-    
-    // The dummy class constructor
-    Snake.Class = function () {
-      // All construction is actually done in the init method
-      if ( !initializing && this.init )
-        this.init.apply(this, arguments);
-    }
-    
-    // Populate our constructed prototype object
-    Snake.Class.prototype = prototype;
-    
-    // Enforce the constructor to be what we expect
-    Snake.Class.constructor = Snake.Class;
-
-    // And make this class extendable
-    Snake.Class.extend = arguments.callee;
-    
-    return Snake.Class;
-  };
-})();
-
 /*
   Initializes Snake with a schema, connects to the database and creates necessary tables.
   @param o Object
@@ -260,35 +192,45 @@ Snake.insertSql = function () {
 /*
   Base Class for the ORM
 */
-Snake.Base = Snake.Class.extend({
+Snake.Base = function (peer, prop) {
 
-  // adds the peer to the model
-  init: function (peer) {
-    this.peer = peer;
-  },
+  var Model = function () { };
+  Model.prototype = {
 
-  // saves a record in the database
-  save: function (onSuccess, onFailure) {
-    this.peer.doUpdate(this, onSuccess, onFailure);
-  },
+    peer: peer,
 
-  /*
-    Hydrates the model with data from the object, usually returned in a transaction
-    @param obj Object
-  */
-  hydrate: function (obj) {
-    for (var i in obj) {
-      if (obj.hasOwnProperty(i)) {
-        this[i] = obj[i];
+    // saves a record in the database
+    save: function (onSuccess, onFailure) {
+      console.log(this);
+      //this.peer.doUpdate(this, onSuccess, onFailure);
+    },
+
+    hydrate: function (obj) {
+      for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+          this[i] = obj[i];
+        }
       }
-    }
-  },
+    },
 
-  // deletes a record from the database
-  remove: function (onSuccess, onFailure) {
-    this.peer.doDeleteRecord(this, onSuccess, onFailure);
+    // deletes a record from the database
+    remove: function (onSuccess, onFailure) {
+      this.peer.doDeleteRecord(this, onSuccess, onFailure);
+    }
+  };
+
+  var prototype = new Model();
+    
+  // Copy the properties over onto the new prototype
+  for (var name in prop) {
+    prototype[name] = prop[name];
   }
-});
+   
+  // Populate our constructed prototype object
+  Model.prototype = prototype;
+
+  return Model;
+};
 
 /*
   Hydrates a recordset from the database into it's respective models
@@ -322,13 +264,16 @@ Snake.hydrateRS = function (peer, callback, transaction, results) {
   @param obj Object
 */
 Snake.BasePeer = function (obj) {
+
+  var Peer = { };
+
   for (var i in obj) {
     if (obj.hasOwnProperty(i)) {
-      this[i] = obj[i];
+      Peer[i] = obj[i];
     }
   }
 
-  return this;
+  return Peer;
 };
 
 Snake.BasePeer.prototype = {
