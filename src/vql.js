34e4cc64ee45e708eaa2f602a132195bdbd6adac
Josh Perez <josh@bigstar.tv>
2011-02-14 17:04:26 -0500

    more vql, need to merge into jasmine

--- src/vql.js
+++ src/vql.js
@@ -1,86 +1,173 @@
 Snake.VQL = {
+  EQUAL: "=", 
+  NOT_EQUAL: "<>",
+  GREATER_THAN: ">", 
+  LESS_THAN: "<", 
+  GREATER_EQUAL: ">=", 
+  LESS_EQUAL: "<=",
+  ISNULL: "IS NULL",
+  ISNOTNULL: "IS NOT NULL",
+  LIKE: "LIKE",
+  NOTLIKE: "NOT LIKE",
+  "IN": "IN",
+  NOTIN: "NOT IN",
+  LEFT_JOIN: "LEFT JOIN",
 
-  _venom: function (table) {
-    var vql = {
-      sql: {
-        select: [],
-        from: table,
-        where: [],
-        orderBy: [],
-        limit: false
+  _venom: function (table, schema) {
+
+    var Base = {
+      limit: function (context, limit) {
+        context.sql.limit = limit;
+        return context;
       },
 
-      find: function (obj) {
-        for (var i in obj) {
-          this.sql.where.push(i + " = ?");
-        }
-
-        return this;
+      offset: function (context, offset) {
+        context.sql.offset = offset;
+        return context;
       },
 
-      orderBy: function (obj) {
+      orderBy: function (context, obj) {
         for (var column in obj) {
-          this.sql.orderBy.push(column + " " + obj[column].toUpperCase());
+          context.sql.orderBy.push(column + " " + obj[column].toUpperCase());
         }
 
-        return this;
+        return context;
       },
 
-      offset: function (offset) {
-        this.sql.offset = offset;
-        return this;
-      },
-
-      limit: function (limit) {
-        this.sql.limit = limit;
-        return this;
-      },
-
-      toSQL: function () {
-
+      toSQL: function (self) {
         var sql = "SELECT #{select} FROM #{from}"
           , query = {};
 
         // SELECT
         query.select = "*";
 
+        if (self.sql.select.length === 0) {
+          query.select = "*";
+/*
+          query.select = [];
+          for (var column in schema.columns) {
+            query.select.push(table + "." + column);
+          }
+        } else {
+*/
+        } else {
+          query.select = self.sql.select;
+        }
+
         // FROM
         query.from = table;
 
         // WHERE
-        if (this.sql.where.length > 0) {
+        if (self.sql.where.length > 0) {
           sql = sql + " WHERE #{where}";
-          query.where = this.sql.where.join(" AND ");
+          query.where = self.sql.where.join(" AND ");
         }
     
         // ORDER BY
-        if (this.sql.orderBy.length > 0) {
+        if (self.sql.orderBy.length > 0) {
           sql = sql + " ORDER BY #{orderBy}";
-          query.orderBy = this.sql.orderBy;
+          query.orderBy = self.sql.orderBy;
         }
 
         // LIMIT && OFFSET
-        if (this.sql.limit) {
-          if (this.sql.offset) {
+        if (self.sql.limit) {
+          if (self.sql.offset) {
             sql = sql + " LIMIT #{offset}, #{limit}";
-            query.offset = this.sql.offset;
+            query.offset = self.sql.offset;
           } else {
             sql = sql + " LIMIT #{limit}";
           }
 
-          query.limit = this.sql.limit;
+          query.limit = self.sql.limit;
         }
 
         return sql.interpose(query);
       }
+
     };
 
-    return vql;
+    var methods = {
+      find: function () {
+
+        var VQL = {
+          sql: {
+            select: [],
+            from: table,
+            where: [],
+            orderBy: [],
+            limit: false
+          },
+
+          orderBy: function (obj) {
+            return Base.orderBy(this, obj);
+          },
+  
+          offset: function (offset) {
+            return Base.offset(this, offset);
+          },
+
+          limit: function (limit) {
+            return Base.limit(this, limit);
+          },
+
+          toSQL: function () {
+            return Base.toSQL(this);
+          }
+        };
+
+          var field = null
+            , value = null
+            , selector = null;
+
+        if (arguments.length > 1) {
+          field = arguments[0]
+          value = arguments[1]
+          selector = arguments[2] || Snake.VQL.EQUAL;
+  
+          VQL.sql.where.push(field + " " + selector + " ?");
+        } else {
+          for (field in arguments[0]) {
+            value = arguments[0][field];
+
+            switch (Object.prototype.toString.call(value)) {
+            case "[object Array]":
+              selector = Snake.VQL.IN;
+              break;
+            case "[object RegExp]":
+              selector = Snake.VQL.LIKE;
+              //console.log(value.toString()); // TODO
+              break;
+            default:
+              selector = Snake.VQL.EQUAL
+            }
+
+            // IN || NOT IN
+            if (selector === Snake.VQL.IN || selector === Snake.VQL.NOTIN) {
+              var q = [];
+
+              for (var i = 0; i < value.length; i = i + 1) {
+                q.push("?");
+              }
+
+              VQL.sql.where.push(field + " " + selector + " (" + q.join(", ") + ")");
+            } else {
+              VQL.sql.where.push(field + " " + selector + " ?");
+            }
+
+          }
+        }
+
+        return VQL;
+      }
+
+    };
+
+    return methods;
   },
 
   _createPeer: function (schema, onSuccess) {
     for (var table in schema) {
-      this[schema[table].jsName] = new this._venom(table);
+      this[schema[table].jsName] = new this._venom(table, schema[table]);
     }
 
     if (onSuccess) {
@@ -106,10 +193,45 @@ var Card = {"deck_id":null,"face":null,"suit":null,"id":null,"created_at":null};
 var PlayerCard = {"player_id":null,"card_id":null,"id":null,"created_at":null};
 
 (function () {
-  Venom = Snake.VQL;
+  var Venom = vql = Snake.VQL;
+
   Venom._createPeer({"player":{"jsName":"Player","columns":{"name":{"type":"text"},"chips":{"type":"integer"},"id":{"type":"INTEGER","primaryKey":true},"created_at":{"type":"INTEGER"}}},"deck":{"jsName":"Deck","columns":{"name":{"type":"text"},"id":{"type":"INTEGER","primaryKey":true},"created_at":{"type":"INTEGER"}}},"card":{"jsName":"Card","columns":{"deck_id":{"type":"integer","foreign":"deck.id"},"face":{"type":"text"},"suit":{"type":"text"},"id":{"type":"INTEGER","primaryKey":true},"created_at":{"type":"INTEGER"}}},"player_card":{"jsName":"PlayerCard","columns":{"player_id":{"type":"integer","foreign":"player.id"},"card_id":{"type":"integer","foreign":"card.id"},"id":{"type":"INTEGER","primaryKey":true},"created_at":{"type":"INTEGER"}}}}, function () {
-    //console.log(venom.Player);
-    console.log(Venom.Player.find({ name: 'Josh' }).orderBy({ name: 'desc' }).limit(5).toSQL());
-    console.log(Venom.Card.limit(10).toSQL());
+
+    //console.log(Venom.Player.find({ name: 'Josh' }).orderBy({ name: 'desc' }).limit(5).toSQL());
+    //console.log(Venom.Card.limit(10).toSQL());
+
+    // SELECT * FROM card WHERE face = 'A' LIMIT 10; // should clear limit
+    console.log(vql.Card.find('face', 'A').limit(10).toSQL());
+
+    // SELECT * FROM card WHERE face = 'A';
+    console.log(vql.Card.find({ face: 'A' }).toSQL());
+
+    // SELECT * FROM card WHERE face > 'A';
+    console.log(vql.Card.find('face', 'A', vql.GREATER_THAN).toSQL());
+
+    // SELECT * FROM card WHERE face = 'A' AND suit = 'hearts';
+    console.log(vql.Card.find({ face: 'A', suit: 'hearts' }).toSQL());
+
+    // SELECT * FROM card WHERE face IN ('A', 'J');
+    console.log(vql.Card.find({ face: ['A', 'J'] }).toSQL());
+
+    // SELECT * FROM player WHERE name LIKE 'josh'; // FIXME
+    console.log(vql.Player.find({ name: /Josh/ }).toSQL());
+
+/*
+// SELECT * FROM card WHERE (face = 'A' and suit = 'hearts') OR (face = 'J' and suit = 'spades');
+console.log(vql.Card.find({
+  or: {
+    and: [{
+      face: 'a',
+      suit: 'hearts'
+    }, {
+      face: 'j',
+      suit: 'spades'
+    }]
+  }
+}).toSQL())
+*/
+
   });
 })();
