f79d2bd7f587f1bc97f178fa8e4ea0ceebe55ca3
Josh Perez <josh@goatslacker.com>
2011-03-30 00:28:15 -0400

    create tables if they don't exist

--- src/snake.js
+++ src/snake.js
@@ -41,59 +41,6 @@ var Snake = {
   },
 
   /**
-    * Dynamically builds the Models
-    *
-    * @param {Object} schema The schema in JSON format
-    * @param {Function} onComplete The callback function to execute once the schema finishes building
-    */
-  loadFromJSON: function (schema, onComplete) {
-    var table = null,
-        column = null,
-        def_column = null,
-        fk = null,
-        model = null;
-
-    for (table in schema) {
-      if (schema.hasOwnProperty(table)) {
-        model = schema[table];
-
-        model.jsName = table;
-        model.columns.id = { type: "INTEGER" };
-        model.columns.created_at = { type: "TIME" };
-
-        model.map = [];
-        for (column in schema[table].columns) {
-          if (schema[table].columns.hasOwnProperty(column)) {
-            def_column = schema[table].columns[column];
-
-            if ("foreign" in def_column) {
-              if (!model.foreign) {
-                model.foreign = {};
-              }
-
-              fk = def_column.foreign.split(".");
-              model.foreign[fk[0]] = [column, fk[1]];
-            }
-
-            model.map.push(column);
-          }
-        }
-
-        // TODO create relationships for the base objects
-        // TODO create doSelectJoins for the relationships
-
-        Snake.venom[schema[table].tableName] = new Snake.venomousObject(model);
-        Snake.global[table] = new Snake.base(model);
-      }
-    }
-
-    if (onComplete) {
-      onComplete();
-    }
-
-  },
-
-  /**
     * Tests whether an Object is an array or not
     *
     * @param {Array} arrayInQuestion The object to check
@@ -102,10 +49,4 @@ var Snake = {
   is_array: function (arrayInQuestion) {
     return (Object.prototype.toString.call(arrayInQuestion) === '[object Array]');
   }
-};
-
-// dummy func
-Snake.define = function (conf) {
-  var self = Snake;
-  self.config.database = conf;
 };
--- src/web.db.js
+++ src/web.db.js
@@ -97,3 +97,105 @@ Snake.query = (function () {
 
   return Query;
 }());
+
+/**
+  * Dynamically builds the Models
+  *
+  * @param {Object} schema The schema in JSON format
+  * @param {Function} onComplete The callback function to execute once the schema finishes building
+  * @param {boolean} createTables If set the true the tables will be automatically created for you if they don't exist
+  */
+Snake.loadFromJSON = function (schema, onComplete, createTables) {
+  var table = null,
+      column = null,
+      def_column = null,
+      fk = null,
+      models = [],
+      model = null;
+
+  for (table in schema) {
+    if (schema.hasOwnProperty(table)) {
+      model = schema[table];
+
+      model.jsName = table;
+      model.columns.id = { type: "INTEGER" };
+      model.columns.created_at = { type: "INTEGER" }; // based off of sqlite 3
+
+      model.map = [];
+      for (column in schema[table].columns) {
+        if (schema[table].columns.hasOwnProperty(column)) {
+          def_column = schema[table].columns[column];
+
+          if ("foreign" in def_column) {
+            if (!model.foreign) {
+              model.foreign = {};
+            }
+
+            fk = def_column.foreign.split(".");
+            model.foreign[fk[0]] = [column, fk[1]];
+          }
+
+          model.map.push(column);
+        }
+      }
+
+      // TODO create relationships for the base objects
+      // TODO create doSelectJoins for the relationships
+     
+      models.push(model);
+
+      Snake.venom[schema[table].tableName] = new Snake.venomousObject(model);
+      Snake.global[table] = new Snake.base(model);
+    }
+  }
+
+  function sqlCreateTables(models) {
+    var queries = [],
+        i = 0,
+        max = 0,
+        column = null,
+        foreign = null,
+        fields = [],
+        fk = [];
+
+    for (i, max = models.length; i < max; i = i + 1) {
+      fields = [];
+      fk = [];
+
+      for (column in models[i].columns) {
+        if (models[i].columns.hasOwnProperty(column)) {
+          if (column !== "id" && column !== "created_at") {
+            fields.push(column + " " + models[i].columns[column].type);
+          }
+        }
+      }
+
+      // FIXME - multiple foreign keys?
+      if ("foreign" in models[i]) {
+        for (foreign in models[i].foreign) {
+          if (models[i].foreign.hasOwnProperty(foreign)) {
+            fk.push("FOREIGN KEY (" + models[i].foreign[foreign][0] + ") REFERENCES " + foreign + "(" + models[i].foreign[foreign][1] + ")");
+          }
+        }
+      }
+
+      fields = fields.concat(["id INTEGER PRIMARY KEY", "created_at INTEGER"]);
+      
+      queries.push(Snake.interpolate("CREATE TABLE IF NOT EXISTS '#{table}' (#{fields} #{refs})", {
+        table: models[i].tableName,
+        fields: fields,
+        refs: fk
+      }));
+    }
+
+    Snake.query(queries, null, onComplete);
+  }
+
+  if (createTables === true) {
+    sqlCreateTables(models);
+  } else {
+    if (onComplete) {
+      onComplete();
+    }
+  }
+};
